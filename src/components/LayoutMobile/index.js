dbe16a829e26558f7d151ead8c691ddf54e5ab23
zannujulius <zannujulius14@gmail.com>
2023-03-06 21:26:06 +0100

    fixed layout screen views

--- src/components/LayoutMobile/index.js
+++ src/components/LayoutMobile/index.js
@@ -1,5 +1,9 @@
 const LayoutMobile = ({ children }) => {
-  return <div className="w-screen h-screen px-3 pt-4">{children}</div>;
+  return (
+    <div className="w-screen h-screen px-3 lg:hidden md:hidden block pt-4">
+      {children}
+    </div>
+  );
 };
 
 export default LayoutMobile;
