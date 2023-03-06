fa062d4ef15c3cebc0cfd4b4839a5a174204f8b2
zannujulius <zannujulius14@gmail.com>
2023-03-06 21:23:10 +0100

    made layout show on flex

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -5,7 +5,7 @@ import TopNav from "../TopNav";
 
 const Layout = ({ children }) => {
   return (
-    <div className="h-screen w-screen fixed items-start top-0 hidden md:flex lg:hidden left-0">
+    <div className="h-screen w-screen fixed items-start top-0 hidden md:flex lg:flex left-0">
       <SideNav />
       <div
         className="h-screen relative top-0 left-0 overflow-y-scroll"
