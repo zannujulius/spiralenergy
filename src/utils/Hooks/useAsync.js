9eab9bd9d3470e932d0abc827bed81f128dffa0e
Ebezer Igbinoba <igbinobaebenezer@gmail.com>
2023-03-07 14:14:58 +0100

    chore: added useSafeDispatch hook

--- src/utils/Hooks/useAsync.js
+++ src/utils/Hooks/useAsync.js
@@ -1,20 +1,9 @@
 import * as React from "react";
-
-function useSafeDispatch(dispatch) {
-  const mounted = React.useRef(false);
-  React.useLayoutEffect(() => {
-    mounted.current = true;
-    return () => (mounted.current = false);
-  }, []);
-  return React.useCallback(
-    (...args) => (mounted.current ? dispatch(...args) : void 0),
-    [dispatch]
-  );
-}
+import {useSafeDispatch} from "./useSafeDispatch"
 
 const defaultInitialState = { status: "idle", data: null, error: null };
 
-function useAsync(initialState) {
+const  useAsync = (initialState) => {
   const initialStateRef = React.useRef({
     ...defaultInitialState,
     ...initialState,
