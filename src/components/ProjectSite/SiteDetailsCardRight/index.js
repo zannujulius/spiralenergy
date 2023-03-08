0289901aed40c8810824cbefdb6a2f915172793c
Ebezer Igbinoba <igbinobaebenezer@gmail.com>
2023-03-08 23:42:30 +0100

    chore: fixed css issues with tailwind classes

--- src/components/ProjectSite/SiteDetailsCardRight/index.js
+++ src/components/ProjectSite/SiteDetailsCardRight/index.js
@@ -20,18 +20,54 @@ const SiteDetailsCardRight = ({
 }) => {
   const { Option } = Select;
   return (
-    <div className=" p-3 lex items-center justify-center flex-col">
-      <div className="row d-flex w-100 align-items-start justify-content-between">
-        <div className="col-sm-12 col-md-8 col-lg-7 col-xl-7 col-xxl-7 mt-2">
+    <div className="container p-3 flex align-center justify-center flex-col">
+      <div className="w-full flex align-center justify-between bg-white">
+        <div
+          className=" mt-2 site-card_title"
+          style={{
+            fontSize: "16px",
+            fontWeight: "500",
+          }}
+        >
+          {title}
+        </div>
+        <div className="w-1/3 mt-2">
+          <div className="details-btn__card">
+            <Select
+              size="medium"
+              style={{
+                width: "100%",
+              }}
+              placeholder={"Select date."}
+            >
+              {[
+                { id: 1, item: "Today" },
+                { id: 2, item: "Week" },
+                { id: 3, item: "Month" },
+                {
+                  id: 4,
+                  item: "Year",
+                },
+              ].map(({ id, item }) => (
+                <Option key={id} value={item}>
+                  {item}
+                </Option>
+              ))}
+            </Select>
+          </div>
+        </div>
+      </div>
+      <div className="flex w-full items-start justify-between">
+        <div className=" w-1/2 mt-2">
           <div
-            className="site-card_right-cover  container p-2 w-100 flex align-center justify-between"
+            className="site-card_right-cover  container-fluid p-3 w-full flex align-center justify-between"
             style={{
               background: bg || color.secondaryColor,
               border: border || "none",
             }}
           >
             <div
-              className="d-flex col-3 align-items-center justify-content-between "
+              className="flex w-1/3 align-center justify-between "
               style={{
                 height: "50px",
               }}
@@ -71,13 +107,13 @@ const SiteDetailsCardRight = ({
                 />
               )}
             </div>
-            <div className="w-50 col-6 d-flex flex-column align-items-end justify-content-end">
+            <div className="w-1/2 flex flex-col items-end justify-end">
               <div className="site-card_right-value">{value}</div>
               <div className="site-card_right-caption mt-2">{caption}</div>
             </div>
           </div>
         </div>
-        <div className="col-sm-12 col-md-4 col-lg-4 col-xl-4 col-xxl-4 mt-2">
+        <div className="w-1/3 mt-2">
           <div className="details-btn__card">
             <Link to={link || "/"} className="text-decoration-none">
               <div
@@ -89,42 +125,6 @@ const SiteDetailsCardRight = ({
                 View details
               </div>
             </Link>
-          </div>
-        </div>
-      </div>
-      <div className="row w-100 d-flex align-items-center justify-content-between bg-white">
-        <div
-          className="col-sm-12 col-md-6 col-lg-6 col-xl-7 col-xxl-6 mt-2 site-card_title"
-          style={{
-            fontSize: "16px",
-            fontWeight: "500",
-          }}
-        >
-          {title}
-        </div>
-        <div className="col-sm-8 col-md-6 col-lg-4 col-xl-4 col-xxl-4 mt-2">
-          <div className="details-btn__card">
-            <Select
-              size="medium"
-              style={{
-                width: "100%",
-              }}
-              placeholder={"Select date."}
-            >
-              {[
-                { id: 1, item: "Today" },
-                { id: 2, item: "Week" },
-                { id: 3, item: "Month" },
-                {
-                  id: 4,
-                  item: "Year",
-                },
-              ].map(({ id, item }) => (
-                <Option key={id} value={item}>
-                  {item}
-                </Option>
-              ))}
-            </Select>
           </div>
         </div>
       </div>
